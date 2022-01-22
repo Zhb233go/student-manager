683325579eaa9c92363eed418f777e98ea85e90f
hypo zhou <2925376741@qq.com>
2022-01-22 16:10:24 +0800

    init commit

--- middleware/logs.go
+++ middleware/logs.go
@@ -7,19 +7,20 @@ import (
 	"os"
 )
 
-func Logger(app *fiber.App) {
-	app.Use(logger.New(logger.Config{
-		Format:     "${pid} ${status} - ${method} ${path}\n",
-		TimeFormat: "02-Jan-2006",
-		TimeZone:   "China/shanghai",
-	}))
+// Logger 日志打印
+func Logger(app *fiber.App) func() {
 	file, err := os.OpenFile("./info.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
-	defer file.Close()
-
 	app.Use(logger.New(logger.Config{
-		Output: file,
+		Format:     "${time}  ${status} - ${method} ${path}\n",
+		TimeFormat: "2006-01-02T15:04:05",
+		TimeZone:   "local",
+		Output:     file,
 	}))
+	//文件关闭接口返回上一层
+	return func() {
+		file.Close()
+	}
 }
